0293e02c62d8124cb3814c180bb5af0034b4a1ce
sajal gupta <gupta.sajal631@gmail.com>
2016-08-01 14:40:33 +0530

    optimization

--- app/src/main/java/tech/salroid/filmy/Activity/FullScreenImage.java
+++ app/src/main/java/tech/salroid/filmy/Activity/FullScreenImage.java
@@ -1,27 +1,32 @@
 package tech.salroid.filmy.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
+import tech.salroid.filmy.Custom.BreathingProgress;
 import tech.salroid.filmy.R;
 
 public class FullScreenImage extends AppCompatActivity {
 
     private String image_url;
     private ImageView centreimg;
+    private BreathingProgress breathingProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image);
+
+        breathingProgress  = (BreathingProgress) findViewById(R.id.breathingProgress);
+        breathingProgress.setVisibility(View.VISIBLE);
+
 
         centreimg = (ImageView) findViewById(R.id.cen_img);
 
@@ -32,8 +37,13 @@ public class FullScreenImage extends AppCompatActivity {
 
         Glide.with(this)
                 .load(image_url)
-                .fitCenter()
-                .into(centreimg);
+                .asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                centreimg.setImageBitmap(resource);
+                breathingProgress.setVisibility(View.INVISIBLE);
+            }
+        });
 
     }
 

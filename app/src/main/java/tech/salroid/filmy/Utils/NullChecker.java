afea56d42a8cbd615e638ca3041e39869b08c0e1
webianks <webianks@gmail.com>
2016-08-10 15:49:30 +0530

    Optimisations in packages name. #5

--- app/src/main/java/tech/salroid/filmy/Utils/NullChecker.java
+++ app/src/main/java/tech/salroid/filmy/Utils/NullChecker.java
@@ -1,14 +1,10 @@
 package tech.salroid.filmy.utils;
 
-/**
- * Created by R Ankit on 07-08-2016.
- */
+public class NullChecker {
 
-public class NullChecker
-  {
-      public static <T> boolean isSettable(T stuff){
+    public static <T> boolean isSettable(T stuff) {
 
-          return !(stuff == null || stuff.equals("null"));
-      }
+        return !(stuff == null || stuff.equals("null"));
+    }
 
 }

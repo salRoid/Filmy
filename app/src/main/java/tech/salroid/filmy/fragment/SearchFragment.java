ae37220235b9d4485bb7739e8c705800fca07d47
sajal gupta <gupta.sajal631@gmail.com>
2016-10-31 20:04:28 +0530

    Search with trakt - updated (further data is shown through tmdb)

--- app/src/main/java/tech/salroid/filmy/fragment/SearchFragment.java
+++ app/src/main/java/tech/salroid/filmy/fragment/SearchFragment.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -151,7 +152,8 @@ public class SearchFragment extends Fragment implements SearchResultAdapter.Clic
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
-                        // Log.d("webi", response.toString());
+
+                        Log.d("webi", response.toString());
                         parseSearchedOutput(response.toString());
                     }
                 }, new Response.ErrorListener() {

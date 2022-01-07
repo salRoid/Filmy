df68ebdfe32c27a740d82eddd72c194c00625ea5
webianks <webianks@gmail.com>
2022-01-07 17:57:16 +0530

    Refactor Testing Classes

---
+++ app/src/androidTest/java/tech/salroid/filmy/DatabaseTest.kt
@@ -0,0 +1,223 @@
+package tech.salroid.filmy
+
+import android.content.ContentValues
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import tech.salroid.filmy.database.FilmContract
+import tech.salroid.filmy.database.FilmDbHelper
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class DatabaseTest {
+
+    private val mContext = ApplicationProvider.getApplicationContext<Context>()
+
+    @Before
+    fun setUp() {
+        deleteTheDatabase()
+    }
+
+    private fun deleteTheDatabase() {
+        mContext.deleteDatabase(FilmDbHelper.DB_NAME)
+    }
+
+    @Test
+    fun testCreateDb() {
+
+        // build a HashSet of all of the table names we wish to look for
+        // Note that there will be another table in the DB that stores the
+        // Android metadata (db version information)
+        val tableNameHashSet = HashSet<String>()
+        tableNameHashSet.add(FilmContract.MoviesEntry.TABLE_NAME)
+        tableNameHashSet.add(FilmContract.CastEntry.TABLE_NAME)
+        mContext.deleteDatabase(FilmDbHelper.DB_NAME)
+        val db = FilmDbHelper(
+            this.mContext
+        ).writableDatabase
+
+        assertEquals(true, db.isOpen)
+
+        // have we created the tables we want?
+        var c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null)
+        assertTrue(
+            "Error: This means that the database has not been created correctly",
+            c.moveToFirst()
+        )
+
+        // verify that the tables have been created
+        do {
+            tableNameHashSet.remove(c.getString(0))
+        } while (c.moveToNext())
+
+        // if this fails, it means that your database doesn't contain both the location entry
+        // and weather entry tables
+        assertTrue(
+            "Error: Your database was created without both the location entry and weather entry tables",
+            tableNameHashSet.isEmpty()
+        )
+
+        // now, do our tables contain the correct columns?
+        c = db.rawQuery(
+            "PRAGMA table_info(" + FilmContract.MoviesEntry.TABLE_NAME + ")",
+            null
+        )
+        assertTrue(
+            "Error: This means that we were unable to query the database for table information.",
+            c.moveToFirst()
+        )
+
+        // Build a HashSet of all of the column names we want to look for
+        val locationColumnHashSet = HashSet<String>()
+        locationColumnHashSet.add(FilmContract.MoviesEntry._ID)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_ID)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_TITLE)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_YEAR)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_POSTER_LINK)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_BANNER)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_DESCRIPTION)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_TRAILER)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_TAGLINE)
+        locationColumnHashSet.add(FilmContract.MoviesEntry.MOVIE_RATING)
+        val columnNameIndex = c.getColumnIndex("name")
+        do {
+            val columnName = c.getString(columnNameIndex)
+            locationColumnHashSet.remove(columnName)
+        } while (c.moveToNext())
+
+        // if this fails, it means that your database doesn't contain all of the required location
+        // entry columns
+        assertTrue(
+            "Error: The database doesn't contain all of the required location entry columns",
+            locationColumnHashSet.isEmpty()
+        )
+        c = db.rawQuery(
+            "PRAGMA table_info(" + FilmContract.CastEntry.TABLE_NAME + ")",
+            null
+        )
+        assertTrue(
+            "Error: This means that we were unable to query the database for table information.",
+            c.moveToFirst()
+        )
+
+        // Build a HashSet of all of the column names we want to look for
+        val locationColumnHashSetCast = HashSet<String>()
+        locationColumnHashSetCast.add(FilmContract.CastEntry._ID)
+        locationColumnHashSetCast.add(FilmContract.CastEntry.CAST_ID)
+        locationColumnHashSetCast.add(FilmContract.CastEntry.CAST_MOVIE_ID)
+        locationColumnHashSetCast.add(FilmContract.CastEntry.CAST_ROLE)
+        locationColumnHashSetCast.add(FilmContract.CastEntry.CAST_NAME)
+        locationColumnHashSetCast.add(FilmContract.CastEntry.CAST_POSTER_LINK)
+        val columnNameIndexCast = c.getColumnIndex("name")
+        do {
+            val columnName = c.getString(columnNameIndexCast)
+            locationColumnHashSetCast.remove(columnName)
+        } while (c.moveToNext())
+
+        // if this fails, it means that your database doesn't contain all of the required location
+        // entry columns
+        assertTrue(
+            "Error: The database doesn't contain all of the required location entry columns",
+            locationColumnHashSet.isEmpty()
+        )
+        db.close()
+    }
+
+    fun testMoviesTable() {
+
+        // First step: Get reference to writable database
+        val filmDbHelper = FilmDbHelper(mContext)
+        val sqLiteDatabase = filmDbHelper.writableDatabase
+
+
+        // Create ContentValues of what you want to insert
+        // (you can use the createNorthPoleLocationValues if you wish)
+        val contentValues = testValues()
+
+
+        // Insert ContentValues into database and get a row ID back
+        val id = sqLiteDatabase.insert(FilmContract.MoviesEntry.TABLE_NAME, null, contentValues)
+        assertTrue("Insertion is unsuccessful", id != -1L)
+
+
+        // Query the database and receive a Cursor back
+
+        //String[] columns = {WeatherContract.LocationEntry.COLUMN_CITY_NAME};
+        val cursor = sqLiteDatabase.query(
+            FilmContract.MoviesEntry.TABLE_NAME,
+            null, null, null, null, null, null
+        )
+
+
+        // Move the cursor to a valid database row
+        assertTrue(cursor.moveToFirst())
+        cursor.close()
+        sqLiteDatabase.close()
+    }
+
+    fun testCastTable() {
+        val filmDbHelper = FilmDbHelper(mContext)
+        val sqLiteDatabase = filmDbHelper.writableDatabase
+        val contentValuesCast = testValuesForCast()
+        val id1 = sqLiteDatabase.insert(FilmContract.CastEntry.TABLE_NAME, null, contentValuesCast)
+        assertTrue("Insertion is unsuccessful", id1 != -1L)
+
+
+        // Query the database and receive a Cursor back
+
+        //String[] columns = {WeatherContract.LocationEntry.COLUMN_CITY_NAME};
+        val cursor = sqLiteDatabase.query(
+            FilmContract.CastEntry.TABLE_NAME,
+            null, null, null, null, null, null
+        )
+        assertTrue(cursor.moveToFirst())
+        cursor.close()
+        sqLiteDatabase.close()
+    }
+
+    private fun testValuesForCast(): ContentValues {
+        val contentValuesCast = ContentValues()
+        contentValuesCast.put(FilmContract.CastEntry.CAST_ROLE, "Superman")
+        contentValuesCast.put(FilmContract.CastEntry.CAST_MOVIE_ID, "t1234")
+        contentValuesCast.put(FilmContract.CastEntry.CAST_NAME, "clarke Kent")
+        contentValuesCast.put(FilmContract.CastEntry.CAST_ID, "tt89231")
+        contentValuesCast.put(
+            FilmContract.CastEntry.CAST_POSTER_LINK,
+            "http://www.webianks.com/logo.png"
+        )
+        return contentValuesCast
+    }
+
+    private fun testValues(): ContentValues {
+        val testMovieId = "t1234"
+        val testMovieTitle = "XXX - RISING"
+        val testMovieYear = 2013
+        val testLink = "http://www.webianks.com/logo.png"
+        val testMovieRating = 6.6
+        val testMovieDescription = "This is Vin diesel best Upcoming"
+        val testMovieTagline = "The return of xander cage"
+        val testMovieBanner = "http://www.webianks.com/logo.png"
+        val testMovieTrailer = "http://www.webianks.com/logo.png"
+
+        val contentValues = ContentValues()
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_ID, testMovieId)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_TITLE, testMovieTitle)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_YEAR, testMovieYear)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_POSTER_LINK, testLink)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_BANNER, testMovieBanner)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_DESCRIPTION, testMovieDescription)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_TAGLINE, testMovieTagline)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_TRAILER, testMovieTrailer)
+        contentValues.put(FilmContract.MoviesEntry.MOVIE_RATING, testMovieRating.toString())
+        return contentValues
+    }
+
+    companion object {
+        val TAG: String = DatabaseTest::class.java.simpleName
+    }
+}
---
+++ app/src/androidTest/java/tech/salroid/filmy/ProviderTest.kt
@@ -0,0 +1,383 @@
+package tech.salroid.filmy
+
+import android.content.ComponentName
+import android.content.ContentUris
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.runner.AndroidJUnit4
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import tech.salroid.filmy.TestUtilities.Companion.createCastValues
+import tech.salroid.filmy.TestUtilities.Companion.createMovieTestValues
+import tech.salroid.filmy.TestUtilities.Companion.insertCastValues
+import tech.salroid.filmy.TestUtilities.Companion.insertMovieValues
+import tech.salroid.filmy.TestUtilities.Companion.testContentObserver
+import tech.salroid.filmy.TestUtilities.Companion.validateCurrentRecord
+import tech.salroid.filmy.TestUtilities.Companion.validateCursor
+import tech.salroid.filmy.database.FilmContract
+import tech.salroid.filmy.database.FilmDbHelper
+import tech.salroid.filmy.providers.FilmProvider
+
+@RunWith(AndroidJUnit4::class)
+class ProviderTest {
+
+    private val mContext = ApplicationProvider.getApplicationContext<Context>()
+
+    @Before
+    fun setUp() {
+        deleteAllRecords()
+    }
+
+    private fun deleteAllRecords() {
+        deleteAllRecordsFromProvider()
+    }
+
+    @Test
+    private fun deleteAllRecordsFromProvider() {
+        mContext.contentResolver.delete(
+            FilmContract.MoviesEntry.CONTENT_URI,
+            null,
+            null
+        )
+        mContext.contentResolver.delete(
+            FilmContract.CastEntry.CONTENT_URI,
+            null,
+            null
+        )
+        var cursor: Cursor? = mContext.contentResolver.query(
+            FilmContract.MoviesEntry.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+        assertEquals(
+            "Error: Records not deleted from Weather table during delete",
+            0,
+            cursor?.count
+        )
+        cursor?.close()
+        cursor = mContext.contentResolver.query(
+            FilmContract.CastEntry.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        assertEquals(
+            "Error: Records not deleted from Location table during delete",
+            0,
+            cursor?.count
+        )
+        cursor?.close()
+    }
+
+    @Test
+    fun testProviderRegistry() {
+        val pm: PackageManager = mContext.packageManager
+        val componentName = ComponentName(
+            mContext.packageName,
+            FilmProvider::class.java.name
+        )
+        try {
+            val providerInfo = pm.getProviderInfo(componentName, 0)
+            assertEquals(
+                "Error: WeatherProvider registered with authority: " + providerInfo.authority +
+                        " instead of authority: " + FilmContract.CONTENT_AUTHORITY,
+                providerInfo.authority, FilmContract.CONTENT_AUTHORITY
+            )
+        } catch (e: PackageManager.NameNotFoundException) {
+            assertTrue(
+                "Error: WeatherProvider not registered at " + mContext.getPackageName(),
+                false
+            )
+        }
+    }
+
+    @Test
+    fun testGetType() {
+        var type: String? =
+            mContext.contentResolver.getType(FilmContract.MoviesEntry.CONTENT_URI)
+        assertEquals(
+            "Error: type mismatch",
+            FilmContract.MoviesEntry.CONTENT_TYPE, type
+        )
+        val testMovieId = "t1234"
+        type = mContext.contentResolver.getType(
+            FilmContract.MoviesEntry.buildMovieWithMovieId(testMovieId)
+        )
+        assertEquals(
+            "Error: type mismatch ",
+            FilmContract.MoviesEntry.CONTENT_ITEM_TYPE, type
+        )
+        type = mContext.contentResolver.getType(
+            FilmContract.CastEntry.buildCastUriByMovieId(testMovieId)
+        )
+        assertEquals(
+            "Error: type mismatch",
+            FilmContract.CastEntry.CONTENT_TYPE, type
+        )
+        type = mContext.contentResolver.getType(FilmContract.CastEntry.CONTENT_URI)
+        assertEquals(
+            "Error: type mismatch",
+            FilmContract.CastEntry.CONTENT_TYPE, type
+        )
+    }
+
+    fun testBasicMoviesQuery() {
+
+        // insert our test records into the database
+        val dbHelper = FilmDbHelper(mContext)
+        val db = dbHelper.writableDatabase
+        val movieValues: ContentValues = createMovieTestValues()
+        val movieRowId = insertMovieValues(mContext)
+        val castValues: ContentValues = createCastValues()
+        val castRowId = db.insert(FilmContract.CastEntry.TABLE_NAME, null, castValues)
+        assertTrue("Unable to Insert MovieEntry into the Database", movieRowId != -1L)
+        db.close()
+
+        // Test the basic content provider query
+        val movieCursor: Cursor? = mContext.contentResolver.query(
+            FilmContract.MoviesEntry.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        // Make sure we get the correct cursor out of the database
+        movieCursor?.let { validateCursor("testBasicMovieQuery", it, movieValues) }
+    }
+
+    fun testBasicCastQueries() {
+        // insert our test records into the database
+        val dbHelper = FilmDbHelper(mContext)
+        val db = dbHelper.writableDatabase
+        val testValues: ContentValues = createCastValues()
+        val castRowId = insertCastValues(mContext)
+
+        // Test the basic content provider query
+        val castCursor: Cursor? = mContext.contentResolver.query(
+            FilmContract.CastEntry.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        // Make sure we get the correct cursor out of the database
+        castCursor?.let { validateCursor("testBasicCastQueries, cast query", it, testValues) }
+
+        // Has the NotificationUri been set correctly? --- we can only test this easily against API
+        // level 19 or greater because getNotificationUri was added in API level 19.
+        if (Build.VERSION.SDK_INT >= 19) {
+            assertEquals(
+                "Error: Cast Query did not properly set NotificationUri",
+                castCursor?.notificationUri, FilmContract.CastEntry.CONTENT_URI
+            )
+        }
+    }
+
+    private fun testInsertReadProvider() {
+        val movieValues: ContentValues = createMovieTestValues()
+
+        // Register a content observer for our insert.  This time, directly with the content resolver
+        var tco = testContentObserver
+        mContext.contentResolver
+            .registerContentObserver(FilmContract.MoviesEntry.CONTENT_URI, true, tco)
+        val locationUri: Uri? =
+            mContext.contentResolver.insert(FilmContract.MoviesEntry.CONTENT_URI, movieValues)
+
+        // Did our content observer get called?  Students:  If this fails, your insert location
+        // isn't calling getContext().getContentResolver().notifyChange(uri, null);
+        tco.waitForNotificationOrFail()
+        mContext.contentResolver.unregisterContentObserver(tco)
+        val movieRowId = locationUri?.let { ContentUris.parseId(it) }
+
+        // Verify we got a row back.
+        assertTrue(movieRowId != -1L)
+
+        // Data'FilmyAuthenticator inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
+        // the round trip.
+
+        // A cursor is your primary interface to the query results.
+        val cursor: Cursor? = mContext.contentResolver.query(
+            FilmContract.MoviesEntry.CONTENT_URI,
+            null,  // leaving "columns" null just returns all the columns.
+            null,  // cols for "where" clause
+            null,  // values for "where" clause
+            null // sort order
+        )
+        cursor?.let {
+            validateCursor(
+                "testInsertReadProvider. Error validating LocationEntry.",
+                it, movieValues
+            )
+        }
+        val castValues: ContentValues = createCastValues()
+        // The TestContentObserver is a one-shot class
+        tco = testContentObserver
+        mContext.contentResolver
+            .registerContentObserver(FilmContract.CastEntry.CONTENT_URI, true, tco)
+        val castInsertUri: Uri? = mContext.contentResolver
+            .insert(FilmContract.CastEntry.CONTENT_URI, castValues)
+        assertTrue(castInsertUri != null)
+
+        // Did our content observer get called? If this means
+        // ContentProvider isn't calling
+        // getContext().getContentResolver().notifyChange(uri, null);
+        tco.waitForNotificationOrFail()
+        mContext.contentResolver.unregisterContentObserver(tco)
+
+        // A cursor is your primary interface to the query results.
+        val castCursor: Cursor? = mContext.contentResolver.query(
+            FilmContract.CastEntry.CONTENT_URI,  // Table to Query
+            null,  // leaving "columns" null just returns all the columns.
+            null,  // cols for "where" clause
+            null,  // values for "where" clause
+            null // columns to group by
+        )
+        castCursor?.let {
+            validateCursor(
+                "testInsertReadProvider. Error validating CastEntry insert.",
+                it, castValues
+            )
+        }
+    }
+
+    fun testDeleteRecords() {
+        testInsertReadProvider()
+
+        // Register a content observer for our movie delete.
+        val locationObserver = testContentObserver
+        mContext.getContentResolver()
+            .registerContentObserver(FilmContract.MoviesEntry.CONTENT_URI, true, locationObserver)
+
+        // Register a content observer for our cast delete.
+        val weatherObserver = testContentObserver
+        mContext.getContentResolver()
+            .registerContentObserver(FilmContract.CastEntry.CONTENT_URI, true, weatherObserver)
+        deleteAllRecordsFromProvider()
+
+        // If either of these fail, you most-likely are not calling the
+        // getContext().getContentResolver().notifyChange(uri, null); in the ContentProvider
+        // delete.  (only if the insertReadProvider is succeeding)
+        locationObserver.waitForNotificationOrFail()
+        weatherObserver.waitForNotificationOrFail()
+        mContext.getContentResolver().unregisterContentObserver(locationObserver)
+        mContext.getContentResolver().unregisterContentObserver(weatherObserver)
+    }
+
+    fun testBulkInsertInMovie() {
+        /* // first, let'FilmyAuthenticator create a location value
+        ContentValues testValues = TestUtilities.createMovieTestValues();
+        Uri locationUri = mContext.getContentResolver().insert(LocationEntry.CONTENT_URI, testValues);
+        long locationRowId = ContentUris.parseId(locationUri);
+
+        // Verify we got a row back.
+        assertTrue(locationRowId != -1);
+
+        // Data'FilmyAuthenticator inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
+        // the round trip.
+
+        // A cursor is your primary interface to the query results.
+        Cursor cursor = mContext.getContentResolver().query(
+                LocationEntry.CONTENT_URI,
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
+                null  // sort order
+        );
+
+        TestUtilities.validateCursor("testBulkInsert. Error validating LocationEntry.",
+                cursor, testValues);
+
+        // Now we can bulkInsert some weather.  In fact, we only implement BulkInsert for weather
+        // entries.  With ContentProviders, you really only have to implement the features you
+        // use, after all.*/
+        val bulkInsertContentValues = createBulkInsertMovieValues()
+
+        // Register a content observer for our bulk insert.
+        val weatherObserver = testContentObserver
+        mContext.contentResolver
+            .registerContentObserver(FilmContract.MoviesEntry.CONTENT_URI, true, weatherObserver)
+        val insertCount: Int = mContext.contentResolver
+            .bulkInsert(FilmContract.MoviesEntry.CONTENT_URI, bulkInsertContentValues)
+
+        // Students:  If this fails, it means that you most-likely are not calling the
+        // getContext().getContentResolver().notifyChange(uri, null); in your BulkInsert
+        // ContentProvider method.
+        weatherObserver.waitForNotificationOrFail()
+        mContext.contentResolver.unregisterContentObserver(weatherObserver)
+        assertEquals(insertCount, BULK_INSERT_RECORDS_TO_INSERT)
+
+        // A cursor is your primary interface to the query results.
+        val cursor: Cursor? = mContext.contentResolver.query(
+            FilmContract.MoviesEntry.CONTENT_URI,
+            null,  // leaving "columns" null just returns all the columns.
+            null,  // cols for "where" clause
+            null,  // values for "where" clause
+            FilmContract.MoviesEntry.MOVIE_YEAR + " ASC" // sort order == by DATE ASCENDING
+        )
+
+        // we should have as many records in the database as we've inserted
+        cursor?.let { assertEquals(it.count, BULK_INSERT_RECORDS_TO_INSERT) }
+
+        // and let'FilmyAuthenticator make sure they match the ones we created
+        cursor?.moveToFirst()
+        var i = 0
+        while (i < BULK_INSERT_RECORDS_TO_INSERT) {
+            bulkInsertContentValues[i]?.let {
+                cursor?.let { it1 ->
+                    validateCurrentRecord(
+                        "testBulkInsert.  Error validating WeatherEntry $i",
+                        it1, it
+                    )
+                }
+            }
+            i++
+            cursor?.moveToNext()
+        }
+        cursor?.close()
+    }
+
+    fun testUpdateMovie() {
+        //TODO
+    }
+
+    companion object {
+        val TAG = ProviderTest::class.java.simpleName
+        private const val BULK_INSERT_RECORDS_TO_INSERT = 10
+        fun createBulkInsertMovieValues(): Array<ContentValues?> {
+            val returnContentValues = arrayOfNulls<ContentValues>(BULK_INSERT_RECORDS_TO_INSERT)
+            for (i in 0 until BULK_INSERT_RECORDS_TO_INSERT) {
+                val contentValues = ContentValues()
+                contentValues.put(FilmContract.MoviesEntry.MOVIE_ID, "t1234$i")
+                contentValues.put(FilmContract.MoviesEntry.MOVIE_TITLE, "Superman $i")
+                contentValues.put(FilmContract.MoviesEntry.MOVIE_YEAR, 2000 + i)
+                contentValues.put(
+                    FilmContract.MoviesEntry.MOVIE_BANNER,
+                    "http://www.webianks.com/logo$i.png"
+                )
+                contentValues.put(FilmContract.MoviesEntry.MOVIE_RATING, 7.8 + i)
+                contentValues.put(FilmContract.MoviesEntry.MOVIE_DESCRIPTION, "this is superman$i")
+                contentValues.put(FilmContract.MoviesEntry.MOVIE_TAGLINE, "this is jaan$i")
+                contentValues.put(
+                    FilmContract.MoviesEntry.MOVIE_TRAILER,
+                    "http://www.webianks.com/logo$i.png"
+                )
+                returnContentValues[i] = contentValues
+            }
+            return returnContentValues
+        }
+    }
+}

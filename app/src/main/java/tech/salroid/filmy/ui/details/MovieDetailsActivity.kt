e6a117f1473a452f9fa35fada5c6f80a4fd3f7e2
Ramankit Singh <webianks@gmail.com>
2026-07-26 22:44:02 +0530

    Merge `release/3.0.0` into `development` (#167)
    * RoomDB: Fallback to destructive migration.
    
    * Remove hardcoded text.
    
    * Remove usage of ClearTextTraffic.
    
    * Bump version code.

--- app/src/main/java/tech/salroid/filmy/ui/details/MovieDetailsActivity.kt
+++ app/src/main/java/tech/salroid/filmy/ui/details/MovieDetailsActivity.kt
@@ -611,7 +611,7 @@ class MovieDetailsActivity : AppCompatActivity() {
         else {
             var smallTitle = movieTitleHyphen?.lowercase()
             smallTitle = smallTitle?.replace("[^\\d-a-z]".toRegex(), "")
-            val url = "http://www.metacritic.com/movie/$smallTitle"
+            val url = "https://www.metacritic.com/movie/$smallTitle"
 
             if (metaScoreRating != null) {
                 when {
